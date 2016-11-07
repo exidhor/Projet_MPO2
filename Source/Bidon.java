5a22ec597e8acb78572784ad6c06282144394abb
Jipay <jipay.detiege@gmail.com>
2016-11-07 14:00:53 +0100

    Adding the base for the test

---
+++ Source/Bidon.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Jipay on 07/11/2016.
+ */
+public class Bidon {
+
+    public int get2()
+    {
+        return 2;
+    }
+}
--- Test/MainUnitTest.java
+++ Test/MainUnitTest.java
@@ -5,4 +5,17 @@ import org.mockito.Mockito;
 
 public class MainUnitTest {
 
+    @Test
+    public void testJUnit() {
+        Bidon bidon = new Bidon();
+        Assert.assertEquals(bidon.get2(), 2);
+    }
+
+    @Test
+    public void testMockito() {
+        Bidon bidon = Mockito.mock(Bidon.class);
+        Mockito.when(bidon.get2()).thenReturn(3);
+
+        Assert.assertNotEquals(bidon.get2(), 2);
+    }
 }
